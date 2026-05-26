1d2d3fe1f970d3703afd6dfc9c1cfdb0a63279bd
kevin santiago <larrotacuervokevinsantiago@gmail.com>
2026-05-26 18:52:50 +0000

    cambio--

--- app.py
+++ app.py
@@ -10,5 +10,5 @@ def home ():
 def api():
     return {"mensaje": "API activa con Flask y systemd"}
 
-if __name__ == "__main__"
+if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
