bd404dd64ce65fac6b69a6f6670a4e5203feb279
kevinsantiago-stack <larrotacuervokevinsantiago@gmail.com>
2026-06-02 13:56:07 +0000

    Correccion ZeroDivisionError

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
-from flask import Flask 
+from flask import Flask
 
-app = Flask (__name__)
+app = Flask(__name__)
 
 @app.route("/")
-def home ():
+def home():
+    x = 1
     return "Flask funcionando correctamente"
 
 @app.route("/api")
