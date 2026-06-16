be692de9058578f0afcc80d2ddabd0f122a9130b
kevinsantiago <larrotacuervokevinsantiago@gmail.com>
2026-06-16 08:25:37 -0500

    cambio para puerto en despliege

--- appm.py
+++ appm.py
@@ -1,4 +1,8 @@
+
+import os
 from flask import Flask, request, jsonify
+import datetime
+
 
 app = Flask(__name__)
 
@@ -24,3 +28,16 @@ def crear_placa():
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
+
+@app.route('/api/repuestos')
+def get_repuestos():
+    return jsonify({
+        "status": "online",
+        "servidor": "Don Pepe",
+        "hora_servidor": str(datetime.datetime.now()),
+        "inventario": ["filtro de aire", "aceite de motor", "bujías", "pastillas de freno"]
+    })
+if __name__ == "__main__":
+    #lea el puerto de la nube de render o use el puerto 5000 por defecto
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
